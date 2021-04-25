601d1dceedd18d58b64303f09b04466906a1a28b
yassineyahyaoui <muhamedyaseenyahyaoui@gmail.com>
2021-04-25 18:02:28 +0200

    barsha fazet

--- script.js
+++ script.js
@@ -8,15 +8,46 @@ let water = document.getElementById("water");
 let header = document.getElementById("header");
 
 document.addEventListener("scroll", () => {
-    let value = document.scrollY;
+  let value = scrollY;
 
-    text.style.top = 50 + value * -0.5 + "%";
-    bird1.style.top = value * -1.5 + "px";
-    bird1.style.left = value * 2 + "px";
-    bird2.style.top = value * -1.5 + "px";
-    bird2.style.left = value * -5 + "px";
-    btn.style.marginTop = value * 1.5 + "px";
-    rocks.style.top = value * -0.12 + "px";
-    forest.style.top = value * 0.25 + "px";
-    header.style.top = value * 0.5 + "px";
+  text.style.top = 50 + value * -0.5 + "%";
+  bird1.style.top = value * -1.5 + "px";
+  bird1.style.left = value * 2 + "px";
+  bird2.style.top = value * -1.5 + "px";
+  bird2.style.left = value * -5 + "px";
+  btn.style.marginTop = value * 1.5 + "px";
+  rocks.style.top = value * -0.12 + "px";
+  forest.style.top = value * 0.25 + "px";
+  header.style.top = value * 0.5 + "px";
+  console.log(value);
+  if (value > 500) {
+    btn.style.display = "none";
+  }
+});
+
+//burger navigation menu
+
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+//wave
+
+let wave1 = document.getElementById("wave1");
+let wave2 = document.getElementById("wave2");
+let wave3 = document.getElementById("wave3");
+let wave4 = document.getElementById("wave4");
+
+document.addEventListener("scroll", () => {
+  let value = scrollY;
+
+  wave1.style.backgroundPosition = 400 + value * 4 +"px";
+  wave2.style.backgroundPosition = 300 + value * -4 +"px";
+  wave3.style.backgroundPosition = 200 + value * 2 +"px";
+  wave4.style.backgroundPosition = 100 + value * -2 +"px";
 })

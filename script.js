79323f18ec530746c69caedf00154fac4907cd73
yassineyahyaoui <muhamedyaseenyahyaoui@gmail.com>
2021-04-26 15:14:57 +0200

    about section

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let text = document.getElementById("text");
 let bird1 = document.getElementById("bird1");
 let bird2 = document.getElementById("bird2");
-let btn = document.getElementById("btn");
+let explore = document.getElementById("explore");
 let rocks = document.getElementById("rocks");
 let forest = document.getElementById("forest");
 let water = document.getElementById("water");
@@ -15,13 +15,16 @@ document.addEventListener("scroll", () => {
   bird1.style.left = value * 2 + "px";
   bird2.style.top = value * -1.5 + "px";
   bird2.style.left = value * -5 + "px";
-  btn.style.marginTop = value * 1.5 + "px";
+  explore.style.marginTop = value * 1.5 + "px";
   rocks.style.top = value * -0.12 + "px";
   forest.style.top = value * 0.25 + "px";
   header.style.top = value * 0.5 + "px";
   console.log(value);
   if (value > 500) {
-    btn.style.display = "none";
+    explore.style.visibility = "hidden";
+  }
+  if (value < 500){
+    explore.style.visibility = "visible";
   }
 });
 
